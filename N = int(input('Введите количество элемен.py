22b9dd33751d00629da69142c03ee4d26b71ec89
Marat <achkych@gmail.com>
2023-03-25 12:04:00 +0400

    Dz001p

--- N = int(input('Введите количество элемен.py
+++ N = int(input('Введите количество элемен.py
@@ -38,4 +38,4 @@
 # elif N == 0:
 # 	print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
 # else:
-#     print('Вы мухлюете и играете не по правилам!')
+#     print('Вы играете не по правилам!')
